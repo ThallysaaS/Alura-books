4dcce36f00167bf552d53bdcd5fb239edb6c281c
Thallys <thallysspeed4@gmai.com>
2023-02-03 12:25:20 -0300

    Subindo projeto

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,9 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src:livro},
+    {nome: 'Liderança em UX', id: 1, src:livro},
+    {nome: 'JavaScript', id: 2, src:livro},
+    {nome: 'Cangaceiro em js', id: 3, src:livro},
+    {nome: 'Aventureiros do java', id: 4, src:livro},
+]
